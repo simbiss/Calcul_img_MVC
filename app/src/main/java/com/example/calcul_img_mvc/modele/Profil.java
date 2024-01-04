b97fd10142f3e4ed705746be5ed41e5d1b45f433
Simon <simonbiss20@gmail.com>
2024-01-04 17:11:32 -0500

    implementation class profil pour calculer l'img de l'utilisateur

--- app/src/main/java/com/example/calcul_img_mvc/modele/Profil.java
+++ app/src/main/java/com/example/calcul_img_mvc/modele/Profil.java
@@ -1,4 +1,88 @@
 package com.example.calcul_img_mvc.modele;
 
+import java.util.Date;
+
 public class Profil {
+
+    //constantes
+    private static final int minFemme = 15;
+    private static final int maxFemme = 30;
+    private static final int minHomme = 10;
+    private static final int maxHomme = 25;
+
+    //proprietés
+    private int poids;
+    private int taille;
+    private int age;
+    private int sexe;
+    private double img;
+    private String message;
+    private Date dateMesure;
+
+    public Profil(Date dateMesure, int poids, int taille, int age, int sexe) {
+        this.poids = poids;
+        this.taille =taille;
+        this.age = age;
+        this.sexe = sexe;
+        this.dateMesure = dateMesure;
+        calculIMG();
+        resulatIMG();
+    }
+
+    //méthode calcule img --> IMG (%) = (1,2 x IMC) + (0,23 x âge) - (10,8 x sexe )
+    private void calculIMG(){
+        double tailleM = (double) taille/100;
+        img = (double)(1.2 * poids / (tailleM * tailleM) + (0.23 * age) - (10.83 * sexe) - 5.4);
+    }
+
+    //méthode retourn le message approprier selon l'img de l'utilisateur
+    private void resulatIMG(){
+        int min;
+        int max;
+        if (sexe == 0){
+            min = minFemme;
+            max = maxFemme;
+        } else {
+            min = minHomme;
+            max = maxHomme;
+        }
+        // message de retour
+        message = "Normal";
+        if (img < min){
+            message = "trop faible";
+        } else {
+            if (img > max) {
+                message = "trop élevé";
+            }
+        }
+    }
+
+    //getter
+    public int getPoids() {
+        return poids;
+    }
+
+    public int getTaille() {
+        return taille;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getSexe() {
+        return sexe;
+    }
+
+    public double getImg() {
+        return img;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Date getDateMesure() {
+        return dateMesure;
+    }
 }
